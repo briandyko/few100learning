f467f90a6714696bdb0493472d9022e2f87d435a
Brian Dyko <briandyko@gmail.com>
2019-02-20 08:51:57 -0500

    wed morning integrating some of m's comments

--- spec/02-functions_spec.ts
+++ spec/02-functions_spec.ts
@@ -4,22 +4,29 @@ describe('functions', () => {
     describe('syntax for creating them', () => {
 
         it('declaring them', () => {
-            // Named Function
+            // Named Functions, when you name a function you can forward reference it in scope, that is why the next line runs
             expect(add(3, 3)).toBe(6);
             function add(a: number, b: number) {
                 return a + b;
             }
 
-            // Anonymous Functions
+            // Anonymous Functions, cannot forward reference them like named functions, moving line 18/22 up would cause them to throw a compiler error
             // -- an anymous function that I immediately invoke
             expect((function (a, b) { return a / b; })(10, 5)).toBe(2)
 
+            // setting an anonymous function expression to be variable that can be invoked like a named function
+
             const multiply = function (a: number, b: number) { return a * b; };
+
+            // creates a function using a lambda, if the only thing in the body of the function is an expression you don't need the return function
+
             const divide = (a: number, b: number) => a / b;
             expect(multiply(3, 3)).toBe(9);
 
 
             expect(divide(10, 2)).toBe(5);
+
+            // the body of the function is more than just an expression, the return keyword is required
 
             const logIt = (msg: string) => {
                 msg = msg.toUpperCase();
